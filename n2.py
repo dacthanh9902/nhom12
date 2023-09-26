9de9d906632c4c61c62937ece29ff1521b7c1bd7
unknown <dacthanh2k2@gmail.com>
2023-09-27 00:53:16 +0700

    test

---
+++ n2.py
@@ -0,0 +1,13 @@
+import tkinter as tk
+
+# Tạo một cửa sổ
+window = tk.Tk()
+
+# Tạo một nhãn
+label = tk.Label(window, text="welcom to ddthanhf")
+
+# Hiển thị nhãn trong cửa sổ
+label.pack()
+
+# Chạy vòng lặp chính của ứng dụng
+window.mainloop()
