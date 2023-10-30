e415db18a909f04f9b422f70828eda13beba2207
Nguyễn Đắc Thành <145109491+dacthanh9902@users.noreply.github.com>
2023-10-30 15:26:47 +0700

    Update cac phep tinh .py

--- bai1,2.py
+++ bai1,2.py
@@ -1,1 +1,11 @@
-asc
+# bài 1 tính tổng, hiệu , tích , thương hai số bất kì :
+try:
+    a = int(input('hay nhap a:'))
+    b = int(input('hay nhap b:'))
+    print('tong = ', a+b)
+    print('hieu =',a-b)
+    print('tich =',a*b)
+    print('thuong = ',a/b)
+except:
+    print(' du lieu nhap khong dung')
+    print("abc")
