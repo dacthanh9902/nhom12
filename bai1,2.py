6dec34a1d8d6a170b046ee0dccd73458c180813e
Hoàng Phương <90737986+coderscool@users.noreply.github.com>
2023-10-30 20:34:03 +0700

    Update bai1,2.py

--- bai1,2.py
+++ bai1,2.py
@@ -1,4 +1,8 @@
 # bài 1 tính tổng, hiệu , tích , thương hai số bất kì :
+import numpy as np
+list = [1,2,4,6,8]
+print(np.sum(list))
+print(np.max(list))
 try:
     a = int(input('hay nhap a:'))
     b = int(input('hay nhap b:'))
